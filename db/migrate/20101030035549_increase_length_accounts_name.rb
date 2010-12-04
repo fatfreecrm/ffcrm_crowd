e931e2cf5afa32d475b302cd7916396e7afa0edc
Nathan B <nathan.f77@gmail.com>
2010-12-04 15:00:57 +0800

    Removed unnecessary migrations and seeds

--- db/migrate/20101030035549_increase_length_accounts_name.rb
+++
@@ -1,9 +0,0 @@
-class IncreaseLengthAccountsName < ActiveRecord::Migration
-  def self.up
-    change_column :accounts, :name, :string, :limit => 128
-  end
-
-  def self.down
-    change_column :accounts, :name, :string, :limit => 64
-  end
-end
--- db/migrate/20101116084037_add_octopus_to_contacts.rb
+++
@@ -1,9 +0,0 @@
-class AddOctopusToContacts < ActiveRecord::Migration
-  def self.up
-    add_column :contacts, :octopus, :string
-  end
-
-  def self.down
-    remove_column :contacts, :octopus
-  end
-end
