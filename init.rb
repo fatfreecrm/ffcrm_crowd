871dcb4068bdc972e42558b3f3ec2db345a3fd5f
Nathan B <nathan.f77@gmail.com>
2010-12-07 12:57:59 +0800

    Plugin is completely disabled in test and cucumber modes.

--- init.rb
+++ init.rb
@@ -1,14 +1,22 @@
 require "fat_free_crm"
 
-FatFreeCRM::Plugin.register(:crm_crowd, self) do
-          name "Crowd"
-        author "Nathan Broadbent"
-       version "1.0"
-   description "Authentication with Atlassian Crowd"
-end
-
 # Disable plugin for test & cucumber environments.
 unless %w(test cucumber).include?(Rails.env)
+
+  FatFreeCRM::Plugin.register(:crm_crowd, self) do
+            name "Crowd"
+          author "Nathan Broadbent"
+         version "1.0"
+     description "Authentication with Atlassian Crowd"
+  end
+
   require "crm_crowd"
   Rails.configuration.middleware.insert_before ::Rack::Lock, ::ActionDispatch::Static, root.join('public') 
+  
+else
+  # Remove view paths if plugin is not initialized.
+  paths = ActionController::Base.view_paths.dup; paths.pop; ActionController::Base.view_paths = paths
 end
+
+
+
