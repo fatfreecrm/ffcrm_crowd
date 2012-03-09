0fd4cd94afa9fe094536783006808d343be45002
Nathan Broadbent <nathan.f77@gmail.com>
2012-03-09 11:11:15 +0800

    Use Setting model for crowd settings

--- config/initializers/crowd.rb
+++ config/initializers/crowd.rb
@@ -1,8 +1,7 @@
 require 'crowd'
 
-begin
-  crowd_settings = YAML.load_file(::Rails.root.join('config/crowd_settings.yml'))
-  crowd_settings["crowd"].each{|k,v| Crowd.send("#{k}=",v) }
-rescue LoadError
+if Setting["crowd"]
+  Setting["crowd"].each{|k,v| Crowd.send("#{k}=",v) }
+else
   puts "Please run '$ ruby install.rb' in vendor/plugins/crm_crowd to setup config/crowd_settings.yml"
 end
