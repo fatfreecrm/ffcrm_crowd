56886be4e8d56ebb32d170ed77a81859cfc4aab1
Nathan Broadbent <nathan.f77@gmail.com>
2012-03-22 11:19:18 +0800

    Use Setting.crowd syntax

--- config/initializers/crowd.rb
+++ config/initializers/crowd.rb
@@ -1,7 +1,7 @@
 require 'crowd'
 
-if Setting["crowd"]
-  Setting["crowd"].each{|k,v| Crowd.send("#{k}=",v) }
+if Setting.crowd
+  Setting.crowd.each{|k,v| Crowd.send("#{k}=",v) }
 else
   puts "Please run '$ ruby install.rb' in vendor/plugins/crm_crowd to setup config/crowd_settings.yml"
 end
