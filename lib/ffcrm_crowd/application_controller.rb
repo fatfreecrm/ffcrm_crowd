ef4ed028473c4529cad6e1bcbc9526aea9b9909a
Steve Kenworthy <steveyken@gmail.com>
2012-11-26 21:21:08 +0800

    Cache current_user during each request. Had been hitting crowd auth server multiple times beforehand.

--- lib/ffcrm_crowd/application_controller.rb
+++ lib/ffcrm_crowd/application_controller.rb
@@ -20,34 +20,38 @@ ApplicationController.class_eval do
 
     #----------------------------------------------------------------------------
     def current_user
-      # Create or return current crowd user as FFCRM User model
-      # (User is pegged via email.)
+      unless @current_user
+        # Create or return current crowd user as FFCRM User model
+        # (User is pegged via email.)
 
-      # Try AuthLogic first (handles HTTP Basic Auth for XML API), fall back to crowd.
-      if current_user_session && current_user_session.respond_to?(:record)
-        @current_user ||= current_user_session.record
-      else
-        if crowd_authenticated?
-          unless user = User.find_by_username(crowd_current_user[:name])
-            # Update username if email is found
-            if user = User.find_by_email(crowd_current_user[:attributes][:mail])
-              user.update_attribute(:username, crowd_current_user[:name])
-            else
-              user = User.create!(:username   => crowd_current_user[:name],
-                                  :email      => crowd_current_user[:attributes][:mail],
-                                  :first_name => crowd_current_user[:attributes][:givenName],
-                                  :last_name  => crowd_current_user[:attributes][:sn])
+        # Try AuthLogic first (handles HTTP Basic Auth for XML API), fall back to crowd.
+        if current_user_session && current_user_session.respond_to?(:record)
+          @current_user ||= current_user_session.record
+        else
+          if crowd_authenticated?
+            unless user = User.find_by_username(crowd_current_user[:name])
+              # Update username if email is found
+              if user = User.find_by_email(crowd_current_user[:attributes][:mail])
+                user.update_attribute(:username, crowd_current_user[:name])
+              else
+                user = User.create!(:username   => crowd_current_user[:name],
+                                    :email      => crowd_current_user[:attributes][:mail],
+                                    :first_name => crowd_current_user[:attributes][:givenName],
+                                    :last_name  => crowd_current_user[:attributes][:sn])
+              end
             end
+
+            @current_user = user
           end
-
-          @current_user ||= user
         end
-      end
 
-      # Set locale to user preference.
-      if @current_user && @current_user.preference[:locale]
-        I18n.locale = @current_user.preference[:locale]
+        # Set locale to user preference.
+        if @current_user
+          @current_user.set_individual_locale
+          @current_user.set_single_access_token
+        end
+        User.current_user = @current_user
       end
-      User.current_user = @current_user
+      @current_user
     end
 end
